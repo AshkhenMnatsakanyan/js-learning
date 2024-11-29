99baea39b6ceda85724fc38a550f4f5f8880d243
Ashkhen Mnatsakanyan <AMnatsakanyan@Ashkhens-MacBook-Pro.local>
2024-11-29 15:30:23 -0800

    Problem 3: Employee Departments

---
+++ 30-11-24/index3.js
@@ -0,0 +1,35 @@
+// Problem 3: Employee Departments
+// Description:
+// You are given an array of employees, where each employee has a name and a department. Your task is to determine how many employees work in each department.
+// Data example
+
+// const employees = [
+//     { name: "Karen", department: "HR" },
+//     { name: "John", department: "IT" },
+//     { name: "Margarita", department: "HR" },
+//     { name: "Aram", department: "Finance" },
+//     { name: "Nina", department: "IT" }
+// ];
+
+
+
+const employees = [
+    { name: "Karen", department: "HR" },
+    { name: "John", department: "IT" },
+    { name: "Margarita", department: "HR" },
+    { name: "Aram", department: "Finance" },
+    { name: "Nina", department: "IT" }
+];
+
+const departmentCount = {};
+
+for (let i = 0; i < employees.length; i++) {
+    let dep = employees[i].department;
+    if (departmentCount[dep]) {
+        departmentCount[dep]++; 
+    } else {
+        departmentCount[dep] = 1; 
+    }
+}
+
+console.log(departmentCount);

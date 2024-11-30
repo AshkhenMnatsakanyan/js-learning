6ba7d37ca57b7be0806cce63c9b6e1c01865d776
Ashkhen Mnatsakanyan <AMnatsakanyan@Ashkhens-MacBook-Pro.local>
2024-11-30 00:48:24 -0800

    Folder renamed

---
+++ 26-11-24/index3.js
@@ -0,0 +1,17 @@
+// Problem 3: Longest Text
+// Given an array of strings ["React", "JavaScript", "Redux", "PHP", "C#"]:
+// Write a program to find the longest word in the array.
+// Solution in JavaScript using only for loop and arrays:
+
+
+const arr = ["React", "JavaScript", "Redux", "PHP", "C#"];
+let longItem = arr[0];
+
+
+for(let i = 1; i < arr.length; i++) {
+    if (arr[i].length > longItem.lenght) {
+        longItem = arr[i]
+    }
+}
+
+console.log(longItem);
